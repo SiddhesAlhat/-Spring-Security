24634e72db742a67f021a17592118954d680a738
Siddhesh Alhat <siddesh5432alhat@gmail.com>
2025-08-23 04:14:26 +0530

    Jws Signup up Verification

---
+++ src/main/java/com/one/verification/BusBookingSystem1Application.java
@@ -0,0 +1,13 @@
+package com.one.verification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusBookingSystem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusBookingSystem1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/one/verification/BusBookingSystem1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.verification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusBookingSystem1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
